36ae8db623863091883d332b4e77515a614cb55c
AryaSadeghi21 <21arya21@gmail.com>
2026-04-09 17:24:42 -0700

    first

--- src/main/java/edu/ucsb/cs156/spring/hello/Developer.java
+++ src/main/java/edu/ucsb/cs156/spring/hello/Developer.java
@@ -20,7 +20,7 @@ public class Developer {
         // You may use just the name that is used on <https://bit.ly/cs156-s26-teams>
         // i.e. your first name, or your first and initial of last name
 
-        return "Chris G.";
+        return "Arya";
     }
 
     /**
@@ -30,7 +30,7 @@ public class Developer {
 
     public static String getGithubId() {
         // TODO: Change this to your github id
-        return "cgaucho";
+        return "AryaSadeghi21";
     }
 
     /**
@@ -40,13 +40,12 @@ public class Developer {
     
     public static Team getTeam() {
         // TODO: Change this to your team name
-        Team team = new Team("s26-xx");
-        team.addMember("Alice");
-        team.addMember("Bob");
-        team.addMember("Chris G.");
-        team.addMember("Danny");
-        team.addMember("Eve");
-        team.addMember("Frances");
+        Team team = new Team("s26-13");
+        team.addMember("Joaquin W");
+        team.addMember("Nick");
+        team.addMember("Pranav");
+        team.addMember("Rohan");
+        team.addMember("Safwan");
         return team;
     }
 }
